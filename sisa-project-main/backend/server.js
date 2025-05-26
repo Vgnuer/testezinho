e2f66e6c9e515e191adf268e89dc7d349ebe47f6
gabriel <ashtar14678@gmail.com>
2025-05-26 00:52:29 -0300

    galinha2

--- sisa-project-main/backend/server.js
+++ sisa-project-main/backend/server.js
@@ -32,12 +32,6 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-const options = {
-  key: fs.readFileSync('./cert/key.pem'),
-  cert: fs.readFileSync('./cert/cert.pem')
-};
-
-// Configuração CORS mais detalhada
 app.use(cors({
   origin: ['https://localhost:3000', 'https://127.0.0.1:3000'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
